dc497691d31a504c189a5744911f9d9c8b9e47e4
quiquelhappy <quiquelhappy@gmail.com>
2019-06-20 23:51:32 +0200

    update debug

--- api/v/1/login/google/index.php
+++ api/v/1/login/google/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../../../../../vendor/autoload.php';
 
 if(isset($_REQUEST["id_token"])){
