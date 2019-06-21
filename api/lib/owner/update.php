01be88186571396c31846313c85a490341fec402
quiquelhappy <quiquelhappy@gmail.com>
2019-06-21 14:11:52 +0200

    owner update debug

--- api/lib/owner/update.php
+++ api/lib/owner/update.php
@@ -1,41 +1,36 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 function updateOwner($email, $name, $surname)
 {
     global $conn;
 
-    $sql = $conn->prepare("SELECT id FROM `owners` WHERE 'email' =:email");
-    $sql->bindValue(":email", $email);
+    $sql = $conn->prepare("SELECT id FROM `owners` WHERE 'email' = :emailid");
+    $sql->bindValue(":emailid", $email);
     $sql->execute();
     $data = $sql->fetch(PDO::FETCH_OBJ);
 
     if (empty($data)) {
         // register acc
 
-        $id=bin2hex(random_bytes(8));
-        $hash=bin2hex(random_bytes(64));
+        $id = bin2hex(random_bytes(8));
+        $hash = bin2hex(random_bytes(64));
 
-        $name=$name; //to-do detox
-        $surname=$surname; //to-do detox
-        $email=$email; //to-do detox
+        $name = $name; //to-do detox
+        $surname = $surname; //to-do detox
+        $email = $email; //to-do detox
 
         $sql = "INSERT INTO owners (`id`, `hash`, `name`, `surname`, `email`) VALUES ('$id', '$hash', '$name','$surname','$email')";
         $conn->exec($sql);
-        
-        return $id;
 
+        return $id;
     } else {
         // update acc
 
-        $id=$data->id;
+        $id = $data->id;
 
-        $name=$name; //to-do detox
-        $surname=$surname; //to-do detox
-        $email=$email; //to-do detox
+        $name = $name; //to-do detox
+        $surname = $surname; //to-do detox
+        $email = $email; //to-do detox
 
         $sql = "UPDATE `owners` SET `email`='$email',`name`='$name',`surname`='$surname' WHERE `id`='$id'";
         $conn->exec($sql);
