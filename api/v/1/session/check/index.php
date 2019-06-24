74cfc08cd36482778d658fcaae68651499cff032
quiquelhappy <quiquelhappy@gmail.com>
2019-06-24 18:29:49 +0200

    fixed typos

--- api/v/1/session/check/index.php
+++ api/v/1/session/check/index.php
@@ -4,15 +4,15 @@
 include_once($_SERVER['DOCUMENT_ROOT']."/api/lib/autoload.php");
 
 if(isset($_REQUEST["session"])){
-    $session=$_REQUEST["sesssion"];
+    $id=$_REQUEST["session"];
 }
 
 if(isset($_REQUEST["session_hash"])){
     $hash=$_REQUEST["session_hash"];
 }
 
-if(isset($session)&&isset($hash)){
-    if(checkSession($session,$hash)){
+if(isset($id)&&isset($hash)){
+    if(checkSession($id,$hash)){
         print(json_encode(array("success"=>true)));
     } else {
         print(json_encode(array("error"=>"invalid credentials")));
