513dffd87fb0b2b8f86e0fc8033c7a58b346bbdb
quiquelhappy <quiquelhappy@gmail.com>
2019-06-27 23:26:55 +0200

    added duplicated server checker

--- api/lib/server/create.php
+++ api/lib/server/create.php
@@ -1,45 +1,57 @@
 <?php
 
-function createServer($name,$ip,$port,$owner){
+function createServer($name, $ip, $port, $owner)
+{
 
     global $conn;
 
-    $name=htmlentities($name, ENT_QUOTES);
+    $name = htmlentities($name, ENT_QUOTES);
 
-    if(!is_null(getOwner($owner))){
-        $name_length=strlen($name);
-        $ip_length=strlen($ip);
-        $port_length=strlen($port);
+    if (!is_null(getOwner($owner))) {
+        $name_length = strlen($name);
+        $ip_length = strlen($ip);
+        $port_length = strlen($port);
 
-        $port_length_ok=($port_length>0&&$port_length<6);
-        $ip_length_ok=($ip_length>3&&$ip_length<30);
-        $name_length_ok=($name_length>3&&$name_length<=32);
+        $port_length_ok = ($port_length > 0 && $port_length < 6);
+        $ip_length_ok = ($ip_length > 3 && $ip_length < 30);
+        $name_length_ok = ($name_length > 3 && $name_length <= 32);
 
-        if($port_length_ok&&$ip_length_ok&&$name_length_ok){
+        if ($port_length_ok && $ip_length_ok && $name_length_ok) {
 
-            if(pingServer($ip,$port)){
+            if (pingServer($ip, $port)) {
 
-                $id = bin2hex(random_bytes(8));
+                $sql = $conn->prepare("SELECT name FROM `servers` WHERE name =:name AND owner = :owner");
+                $sql->bindValue(":name", $name);
+                $sql->bindValue(":owner", $owner);
+                $sql->execute();
+                $data = $sql->fetch(PDO::FETCH_OBJ);
 
-                $port=intval($port);
+                if (empty($data)) {
 
-                $sql = "INSERT INTO servers (`id`, `name`, `ip`, `port`, `owner`) VALUES ('$id', '$name', '$ip','$port','$owner')";
-                $conn->exec($sql);
+                    $id = bin2hex(random_bytes(8));
 
-                return $id; // all ok
+                    $port = intval($port);
 
+                    $sql = "INSERT INTO servers (`id`, `name`, `ip`, `port`, `owner`) VALUES ('$id', '$name', '$ip','$port','$owner')";
+                    $conn->exec($sql);
+
+                    return $id; // all ok
+
+                } else {
+
+                    return null; // the owner already has a server with the same name in that account (with that email)
+
+                }
             } else {
 
                 return null; // couldn't ping the server
 
             }
-
         } else {
 
             return null; // couldn't check the lenghts
 
         }
-
     } else {
 
         return null; // couldn'0t check the owner
